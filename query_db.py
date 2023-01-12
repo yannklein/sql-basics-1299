d59686c6efcfd66a8ef453126067b057722b8999
Yann Klein <yann.klein@me.com>
2023-01-12 17:54:24 +0900

    recap done

--- query_db.py
+++ query_db.py
@@ -1,19 +1,31 @@
 import sqlite3
+import sys 
+import tracemalloc
+
+tracemalloc.start()
+
+print("Mem alloc initial (current, peak)")
+print(tracemalloc.get_traced_memory())
 
 conn = sqlite3.connect('data/database.sqlite')
 conn.row_factory = sqlite3.Row
+
+print("Mem alloc after connection (current, peak)")
+print(tracemalloc.get_traced_memory())
+
 c = conn.cursor()
-c.execute('SELECT * FROM League')
+
+print("Mem alloc before execute (current, peak)")
+print(tracemalloc.get_traced_memory())
+
+c.execute('SELECT * FROM Player_Attributes')
+
+print("Mem alloc after execute (current, peak)")
+print(tracemalloc.get_traced_memory())
+
 rows = c.fetchall()
 
-# print(rows)
-for row in rows:
-    print(row['name'])
-    
-# conn = sqlite3.connect('data/database.sqlite')
-# conn.row_factory = sqlite3.Row
-# c = conn.cursor()
-# c.execute('SELECT * FROM League WHERE id = 1')
-# rows = c.fetchone()
-
-# print(dict(rows))
+print("Mem alloc after fetch all (current, peak)")
+print(tracemalloc.get_traced_memory())
+ 
+tracemalloc.stop()
